7f6304f21e207f87e989cf4c10f568fe71ae5900
Florent Hivert <Florent.Hivert@lri.fr>
2016-07-07 18:00:39 +0200

    Rondelle with only 2 holes

--- BellBall/rondelle.py
+++ BellBall/rondelle.py
@@ -5,12 +5,12 @@ from math import sqrt
 
 unit.set(defaultunit="mm")
 
-nx = 11
-ny = 9
-diametre = 27.0
+nx = 7
+ny = 5
+diametre = 20.0
 trou_central = 5 - .2
-trou_vis = 3.0
-trou_distance=10
+trou_vis = 3.0 - .2
+trou_distance=6
 margin = .3
 style.linewidth.hairthin = style.linewidth(.25)
 cut = [style.linewidth.hairthin, color.rgb.red]
@@ -21,8 +21,8 @@ def rondelle():
     c.stroke(path.circle(0, 0, trou_central/2), cut)
     c.stroke(path.circle(trou_distance,0, trou_vis/2), cut)
     c.stroke(path.circle(-trou_distance,0, trou_vis/2), cut)
-    c.stroke(path.circle(0,trou_distance, trou_vis/2), cut)
-    c.stroke(path.circle(0,-trou_distance, trou_vis/2), cut)
+    #c.stroke(path.circle(0,trou_distance, trou_vis/2), cut)
+    #c.stroke(path.circle(0,-trou_distance, trou_vis/2), cut)
     return c
 
 xoffset = diametre+.3
@@ -38,10 +38,9 @@ print("Largeur: %s, hauteur: %s"%(nx*xoffset, (ny-1)*yoffset+xoffset))
 can = canvas.canvas()
 for x in range(nx):
     for y in range(ny):
-        if (y == 0 and x<3) or y==1 and x<1:
-            continue
         can.insert(rondelle(),
-                 [trafo.translate(xoffset*(x+.5+(oddshift if y %2==1 else 0)), yoffset*(y+.5))])
+                 [trafo.translate(7+xoffset*(x+.5+(oddshift if y %2==1 else 0)),
+                                  7+yoffset*(y+.5))])
 
 import os
 
